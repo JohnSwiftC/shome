b1b7047d09f6a8af9847d29dda07fdbb58c4ed06
JohnSwiftC <johnswift@live.com>
2025-07-16 20:31:51 -0500

    optional log files for search, adds to a list

--- src/core/upnp/search.rs
+++ src/core/upnp/search.rs
@@ -1,30 +1,16 @@
 use std::fs::File;
 use std::io::Write;
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 
 use std::net::UdpSocket;
 
+use crate::core::upnp::UPnPFriendlyIP;
 use crate::{
-    core::{EngineContext, Job},
+    core::{upnp::DeviceManager, EngineContext, Job},
     utils, Command, CommandResult,
 };
 use utils::create_log_file;
-
-fn read_ssdp_to_log(file: &mut File, socket: &mut UdpSocket) {
-    let mut buf = vec![0; 4096];
-    let (bytes, sender) = match socket.recv_from(&mut buf) {
-        Ok((bytes, sender)) => (bytes, sender),
-        Err(e) => {
-            eprintln!("ERROR: failed to read from udp socket, consider kill upnp-search job");
-            return;
-        }
-    };
-    let bufstr = String::from_utf8_lossy(&buf[..bytes]);
-    if let Err(e) = write!(file, "Packet from {}\n{}", sender, bufstr) {
-        eprintln!("ERROR: failed to write to log file, consider killing upnp-search job");
-    }
-}
 
 pub struct UPnPSearch {}
 impl Command for UPnPSearch {
@@ -34,21 +20,21 @@ impl Command for UPnPSearch {
 
     fn info(&self) -> &str {
         "Used to passively read UPnP devices broadcasting themselves\n\
-        over SSDP, and then output those results to a log file.\n\
+        over SSDP, and then output those results to the upnp list.\n\
         Flags:\n\
-        -o or --output <string> : name of log file (default: shomelog-#.txt)"
+        -o or --output <string> : also output to a named log file"
     }
 
     fn run(&self, input: &str, context: &EngineContext) -> Result<CommandResult, CommandResult> {
         let mut args = input.split_whitespace();
-        let mut log_file_name = "shomelog";
-        let mut log_file: File;
+        let mut log_file_name = None;
+        let mut log_file: Option<File>;
 
         while let Some(curr) = args.next() {
             match curr {
                 "-o" | "--output" => {
                     if let Some(n) = args.next() {
-                        log_file_name = n.trim();
+                        log_file_name = Some(n.trim());
                     } else {
                         return Err(CommandResult::Failure {
                             message: "output flag used but no value given".to_owned(),
@@ -64,15 +50,14 @@ impl Command for UPnPSearch {
             }
         }
 
-        log_file = create_log_file(log_file_name)?;
+        log_file = match log_file_name {
+            Some(name) => Some(create_log_file(name)?),
+            None => None,
+        };
+
         let upnp_manager_arc = context.upnp_device_manager.clone();
 
-        // TODO, rewrite the actual functionality here, add flag for optional
-        // log file, ensure killing this job goes smoothly
-        // create shared sockets in the context to prevent the double opening
-        // error
-
-        let mut socket = UdpSocket::bind("0.0.0.0:1900").map_err(|e| CommandResult::Failure {
+        let socket = UdpSocket::bind("0.0.0.0:1900").map_err(|e| CommandResult::Failure {
             message: "udp socket already open at port 1900, consider killing related upnp jobs"
                 .to_owned(),
         })?;
@@ -81,9 +66,32 @@ impl Command for UPnPSearch {
 
         let _ = thread::spawn(move || {
             let mut log_file = log_file;
-            let mut socket = socket;
+            let socket = socket;
             while let Err(_) = receiver.try_recv() {
-                read_ssdp_to_log(&mut log_file, &mut socket);
+                let mut buf = vec![0; 4096];
+                let (bytes, sender) = match socket.recv_from(&mut buf) {
+                    Ok((bytes, sender)) => (bytes, sender),
+                    Err(e) => {
+                        eprintln!("ERROR: failed to read from udp socket, consider kill upnp-search job");
+                        return;
+                    }
+                };
+                let bufstr = String::from_utf8_lossy(&buf[..bytes]);
+
+                if let Some(ref mut f) = log_file {
+                    if let Err(e) = write!(f, "Packet from {}\n{}", sender, bufstr) {
+                        eprintln!("ERROR: failed to write to log file, consider killing upnp-search job");
+                    }
+                }
+
+                {
+                    let mut lock = upnp_manager_arc.lock().expect("failed to lock upnp manager, quitting... (search)");
+                    lock.insert(UPnPFriendlyIP {
+                        ip: sender.ip(),
+                        name: "some device".to_owned(),
+                    });
+                }
+
             }
         });
 
